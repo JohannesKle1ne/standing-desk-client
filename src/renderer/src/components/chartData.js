6ddd963415e6210ba5bbc8a7cca78431bd17072d
JohannesKle1ne <jojo.kleine@gmx.de>
2024-07-29 09:10:38 +0200

    remove +1 from hour display

--- src/renderer/src/components/chartData.js
+++ src/renderer/src/components/chartData.js
@@ -264,7 +264,7 @@ export function getChartDataForDay(initData, timestampStart) {
     const hours = date.getHours()
     return {
       ...g,
-      time: `${hours + 1}:00`
+      time: `${hours}:00`
     }
   })
 
