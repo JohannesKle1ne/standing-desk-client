4396543b577b2ad2e21103134504abef51dc8e93
JohannesKle1ne <jojo.kleine@gmx.de>
2024-08-19 13:55:48 +0200

    fix

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -13,10 +13,11 @@
 import Base from './components/Base.vue'
 import { ref, onMounted, computed } from 'vue'
 const users = ref([
-  { userName: 'jo', id: '86e945c0-e395-4cf6-a8ec-892e904e6940' },
-  { userName: 'Luke Haliburton', id: 'b7244bbe-1d28-4417-91a2-522964a6554f' },
+  /*  { userName: 'jo', id: '86e945c0-e395-4cf6-a8ec-892e904e6940' },
+,
   { userName: 'Teodora Mitrevska', id: '99aca265-fa71-4fa5-9f96-9e93084dd5ea' },
-  { userName: 'Frank', id: 'e085e834-5970-4c44-a34b-84ccc73f354b' },
+  { userName: 'Frank', id: 'e085e834-5970-4c44-a34b-84ccc73f354b' }, */
+  { userName: 'Luke Haliburton', id: 'b7244bbe-1d28-4417-91a2-522964a6554f' },
   { userName: 'Yannick (Yannick)', id: '0cc3946b-6f53-4feb-9048-1b3a1dd6db59' },
   { userName: 'Francesco (Francesco)', id: 'bb678323-1be4-40ea-b95b-748e54649c34' },
   { userName: 'xuedong (Xuedong)', id: '6dd5af42-29b0-48f3-bbb1-3bfcfee1ae10' }
